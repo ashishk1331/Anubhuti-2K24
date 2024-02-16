01900bff4ce937ef80e9403e2c4be077a54de0c6
Ashish Khare <akstyles211@gmail.com>
2024-02-16 15:19:48 +0530

    Scrape Preline in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import PrelineScript from "./components/PrelineScript";
+// import PrelineScript from "./components/PrelineScript";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <PrelineScript />
     </html>
   );
 }
