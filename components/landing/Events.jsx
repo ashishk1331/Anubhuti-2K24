677811c3fe433654dba8ff73d703ebaf1322e7f5
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2024-02-21 17:38:32 +0530

    Redesigned events section

--- components/landing/Events.jsx
+++ components/landing/Events.jsx
@@ -1,19 +1,72 @@
-import React from "react";
-import { EventCard } from "./EventCard";
-export default function Events({ eventsData }) {
+import events from "/public/events/events.json";
+export default function (props) {
   return (
-    <div
-      className="py-10 flex flex-col gap-10 items-center justify-center bg-[#000f1e]  transition-all duration-400 ease-in-out"
-      id="Events"
-    >
-      <h2 className="text-[#efc36a] font-bold text-6xl -mt-[10px] mb-10">
-        All Events
-      </h2>
-      <div className="grid flex-wrap items-center justify-between grid-cols-1 gap-10 px-2 sm:px-2 md:px-0 sm:grid-cols-2 md:grid-cols-3 place-content-stretch">
-        {eventsData.map((eventData) => (
-          <EventCard key={eventData.id} eventData={eventData} />
-        ))}
+    <>
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="max-w-2xl mx-auto mb-10 text-center lg:mb-14">
+          <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+            Events
+          </h2>
+        </div>
+        {/* Grid */}
+        <div className="grid gap-6 lg:grid-cols-2">
+          {events.map((event) => (
+            <Card key={event.id} {...event} />
+          ))}
+        </div>
+        {/* End Grid */}
       </div>
-    </div>
+    </>
+  );
+}
+
+function Card(props) {
+  const { eventName, summary, id } = props;
+
+  let refinedName = eventName;
+  if (refinedName.indexOf("(") > -1) {
+    refinedName = refinedName.substr(0, refinedName.indexOf("("));
+  }
+
+  return (
+    <a
+      className="relative block group rounded-xl dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+      href={`/events/${id}`}
+    >
+      <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full h-[350px] before:absolute before:inset-x-0 before:size-full before:bg-gradient-to-t before:from-gray-900/[.7] before:z-[1]">
+        <img
+          className="absolute top-0 object-cover size-full start-0"
+          src="https://images.unsplash.com/photo-1479813183133-f2e9b38ed6c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1062&q=80"
+          alt="Image Description"
+        />
+      </div>
+      <div className="absolute inset-x-0 top-0 z-10">
+        <div className="flex flex-col h-full p-4 sm:p-6">
+          {/* Avatar */}
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img
+                className="size-[46px] border-2 border-white rounded-full"
+                src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                alt="Image Description"
+              />
+            </div>
+            <div className="ms-2.5 sm:ms-4">
+              <h4 className="font-semibold text-white">Gloria</h4>
+              <p className="text-xs text-white/[.8]">May 30, 2021</p>
+            </div>
+          </div>
+          {/* End Avatar */}
+        </div>
+      </div>
+      <div className="absolute inset-x-0 bottom-0 z-10">
+        <div className="flex flex-col h-full p-4 sm:p-6">
+          <h3 className="text-lg sm:text-3xl font-semibold text-white group-hover:text-white/[.8]">
+            {refinedName}
+          </h3>
+          <p className="mt-2 text-white/[.8]">{summary}</p>
+        </div>
+      </div>
+    </a>
   );
 }
