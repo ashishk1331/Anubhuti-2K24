245928b42d6e93a44cee5fa20fe3a59fe46999f9
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2024-02-26 16:35:37 +0530

    Removed typo in contribute page

--- app/contribute/page.jsx
+++ app/contribute/page.jsx
@@ -16,7 +16,7 @@ export default function (props) {
           <div className="grid gap-4 md:grid-cols-2 md:gap-8 xl:gap-20 md:items-center">
             <div>
               <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">
-                Become a sponser of{" "}
+                Become a sponsor of{" "}
                 <span className="text-voilet">Anubhuti</span>
               </h1>
               <p className="mt-3 text-lg text-gray-800 dark:text-gray-400">
