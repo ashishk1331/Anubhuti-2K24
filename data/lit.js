821fc2ec3246caae28145500d85f8d1270fdb649
Anurag Kumar <anurag.21213@knit.ac.in>
2024-02-29 22:19:03 +0530

    added lit data

---
+++ data/lit.js
@@ -0,0 +1,93 @@
+export const litEvent=[
+    {
+      organizingCouncil: "Literary Council",
+      addedOn: "29 Feb",
+      id: "1c60a7c4-8a98-4b3f-b321-57b08b2997a7",
+      eventName: "Debate",
+      image: odoru_era,
+      category:[
+        {
+            categoryName:"The Parliamentary Masquerade",
+            description:"A typical form of the parliamentary debate with the simplifications of the rules so that every person can simply understand the event and enjoy it. The event will consist of a speaker, ruling side and an opposition.",
+            language:"Bilingual",
+            scopeOfEvent:"Major", 
+            Venue:"CSA", 
+            estimatedDays:"2 or 3",
+            participation:"Individual",
+        },
+        {
+            categoryName:"मंथन",
+            description:"An event consists of two stages: Group Discussion and panel discussion. The event will take place in two stages. The first will be the introductory round, consisting of the group discussion. The top-performing candidates will move to the next round, which will be a panel discussion. ",
+            language:"Hindi",
+            scopeOfEvent:"Major", 
+            Venue:"Seminar Hall and Alumni Park ", 
+            estimatedDays:"2",
+            participation:"Individual",
+        },
+        {
+            categoryName:"Against the For",
+            description:"This will be a non-conventional debate where teams will be assigned a topic and tasked with speaking in favor of it, within a time frame of 4-5 minutes to present their thoughts. At the end of this time, the next team will have to speak in favor of the topic. Moving to the next step, both teams will present their views in favor of the topic, and they will also need to articulate their views against it. Once both teams have spoken for and against the topic, the jury will decide which team presented their views most effectively. ",
+            language:"Hindi",
+            scopeOfEvent:"Major", 
+            Venue:"Seminar Hall", 
+            estimatedDays:"2 or 3",
+            participation:"Team",
+        },
+        {
+            categoryName:"Battle of Rhetoricians ",
+            description:"This will be a ladder event in which each individual participant will be paired with another participant on the day of the event (the pairing will be random). Each pair will be assigned a topic to speak in favor of or against. The favor or opposition will be decided with the chit system. Each participant will give a 3–4-minute speech on their given topic, and after the speeches of both participants, the jury members will decide who will move to the next rounds.Topics will be related to current affairs and general perceptions and similar fields.One of the candidates will be chosen for the next round, and a similar pairing will happen until we get the top 4 participants.  ",
+            language:"English",
+            scopeOfEvent:"Major", 
+            Venue:"Seminar Hall", 
+            estimatedDays:"1",
+            participation:"Individual",
+        },
+      ]
+      
+       
+    },
+    {
+      organizingCouncil: "Literary Council",
+      addedOn: "29 Feb",
+      id: "1c60a7c4-8a98-4b3f-b321-57b08b2997a7",
+      eventName: "Poetry",
+      image: odoru_era,
+      solo:[
+        {
+            eventName:"KavyaSarita (Hindi Poem)",
+            description:"Participant recites his/her self-written poem purely in Hindi (no use of Urdu words).",
+            rounds:[{
+                noOfRounds:2,
+                round1:"Online entry (video entry)",
+                round2:"Stage round",
+            }],
+            venue:"Bsc Ground (1 day event)"
+        },
+        {
+            eventName:"Poetry Slam (English Poem)",
+            description:"Participants recite his/her self-written poem purely in English.",
+            rounds:[{
+                noOfRounds:2,
+                round1:"Online entry (video entry)",
+                round2:"Stage round",
+            }],
+            venue:"CSA Hall(1day event)"
+        },
+        {
+            eventName:"Bait Baazi (Shayari Competition)",
+            description:"Two teams compete with each other in such a way the second team recites a sher starting with the last letter of sher recited by the first team. (First,sher will be given by the host).",
+            rounds:[{
+                noOfRounds:2,
+                round1:"Online entry (video entry)",
+                round2:"Stage round",
+            }],
+            venue:"Alumni Park (2 day event)",
+            
+        },
+      ]
+      
+       
+    },
+    
+  ];
+  
