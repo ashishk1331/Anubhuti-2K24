122de5cc7f79ac2ada79bcfbef280c6b894a8196
Anurag Kumar <anurag.21213@knit.ac.in>
2024-02-29 11:28:29 +0530

    added team section

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,3 +1,44 @@
+
+import { team } from "@/data/team";
+
 export default function () {
-    return <div>teams</div>
+    return <div className="bg-[#000f1e]">
+        <>
+            <div className="w-full h-24 bg-sky-500 bg-gradient-to-b from-[#2b124c] to-[#000f1e]" />
+            <div className="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto ">
+                {/* Title */}
+                <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                    <p className="mt-10 mb-10 text-[#efc36a] font-bold text-7xl ">Our Team</p>
+                </div>
+                {/* End Title */}
+                {/* Grid */}
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 md:gap-12">
+                    {
+                        team.map((member, index) => (
+                            <div className="text-center" key={index}>
+                                <img
+                                    className="rounded-full size-24 mx-auto"
+                                    src={member.image.src}
+                                    alt={member.name}
+                                />
+                                <div className="mt-2 sm:mt-4">
+                                    <h3 className="font-medium text-gray-800 dark:text-gray-200">
+                                        {member.name}
+                                    </h3>
+                                    <p className="text-sm text-gray-600 dark:text-gray-400">
+                                        {member.designation}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
+
+
+                </div>
+                {/* End Grid */}
+              
+            </div>
+
+        </>
+    </div>
 }
---
+++ data/team.js
@@ -0,0 +1,73 @@
+import coConvenor from "../public/team/coConvenor1.jpg"
+import convenor1 from "../public/team/sujeetSir.jpg"
+import maleavatar from "../public/team/maleavatar.png"
+import femaleavatar from "../public/team/femaleavatar.jpg"
+
+export  const team=[
+    {
+        image:convenor1,
+        name:'Samir Srivastav',
+        designation:'Convenor'
+    },
+    {
+        image:convenor1,
+        name:'Sujeet Agarwal',
+        designation:'Convenor'
+    },
+    {
+        image:coConvenor,
+        name:'Bipin Prajapati',
+        designation:'Co-Convenor'
+    },
+    {
+        image:coConvenor,
+        name:'Rajnish Kumar',
+        designation:'Co-Convenor'
+    },
+    {
+        image:maleavatar,
+        name:'Mayank Kesharwani(EL)',
+        designation:'Fest Secretary'
+    },
+    {
+        image:femaleavatar,
+        name:'Deepanshi Srivastava(CE)',
+        designation:'Fest Secretary'
+    },
+    {
+        image:maleavatar,
+        name:'Ayush Dubey(ME)',
+        designation:'Core Team'
+    },
+    {
+        image:maleavatar,
+        name:'Amit Rankwar(EL)',
+        designation:'Core Team'
+    },
+    {
+        image:maleavatar,
+        name:'Harsh Gupta(EL)',
+        designation:'Core Team'
+    },
+    {
+        image:maleavatar,
+        name:'Abhishek Choudhary(EE)',
+        designation:'Core Team'
+    },
+    {
+        image:maleavatar,
+        name:'Amritesh Singh(EL)',
+        designation:'Core Team'
+    },
+    {
+        image:femaleavatar,
+        name:'Apeksha Chauhan(ME)',
+        designation:'Core Team'
+    },
+    {
+        image:femaleavatar,
+        name:'Akansha Walia(CSE)',
+        designation:'Core Team'
+    },
+   
+]
